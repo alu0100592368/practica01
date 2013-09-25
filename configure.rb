7e6c46e8fadb52e2011309c8c28362bd0645509a
Diego Williams Aguilar Montaño <alu0100592368@ull.edu.es>
2013-09-25 12:09:54 +0100

    Quitando el fichero privado

--- configure.rb
+++
@@ -1,6 +0,0 @@
-Twitter.configure do |config|
-  config.consumer_key       = 'iwKedkEgTDnbL3NRrqK3AA'
-  config.consumer_secret    = '56agBriYRRoqgwacpyZRfnkRIXlFTxMUgsPHTkDWs'
-  config.oauth_token        = "51539417-CVQaFUbOUNfD2QTP9V17ALPRgEQLsX5Pg2VXJcFvB"
-  config.oauth_token_secret = "yXBmAxuJwWHEB8NuPP6SaGZuYwys3fKfAN6KUjZw"
-end
